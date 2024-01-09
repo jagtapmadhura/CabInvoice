701ab81392a855d9787ea0a0e46e25e3e3424dd8
jagtapmadhura <madhurajagtap929@gmail.com>
2024-01-09 18:35:52 +0530

    Implemented Test Case for step 2

--- src/test/java/CabInvoiceTest.java
+++ src/test/java/CabInvoiceTest.java
@@ -11,4 +11,12 @@ public class CabInvoiceTest {
         double result = cabInvoice.calculateFare(distance, time);
         Assert.assertEquals(110,result,0);
     }
+
+    @Test
+    public void calculateMultipleFares() {
+        double distance[] = {10,20};
+        int time[] = {10,30};
+        double result = cabInvoice.calculateMultipleFares(distance, time);
+        Assert.assertEquals(340,result,0);
+    }
 }
